9d8c46c7f2ad7968609489a226deafce8f23d93e
Raksani <raksani.k@ku.th>
2019-02-14 14:40:32 +0700

    longer snake

--- snake_.py
+++ snake_.py
@@ -19,6 +19,19 @@ class ModelSprite(arcade.Sprite):
         self.sync_with_model()
         super().draw()
 
+
+class SnakeSprite:
+    def __init__(self, snake):
+        self.snake = snake
+        self.block_sprite = arcade.Sprite('images/block.png')
+
+    def draw(self):
+        for x, y in self.snake.body:
+            # เพิ่มโค้ดที่วาด self.block_sprite ลงตำแหน่ง (x,y) หลักๆ คือให้ set_position ก่อน แล้วค่อย draw
+            self.block_sprite.set_position(x,y)
+            self.block_sprite.draw()
+
+
 class SnakeWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
@@ -28,9 +41,10 @@ class SnakeWindow(arcade.Window):
 
         # self.snake_sprite = arcade.Sprite('images/block.png')
         # we use class Model sprite instead for collect image data.
-        self.snake_sprite = ModelSprite('images/block.png',
-                                        model=self.world.snake)
-        self.snake_sprite.set_position(300, 300)
+        # self.snake_sprite = ModelSprite('images/block.png',
+        #                                 model=self.world.snake)
+        # self.snake_sprite.set_position(300, 300)
+        self.snake_sprite = SnakeSprite(self.world.snake)
         arcade.set_background_color(arcade.color.BLACK)
     # create update for update in world class.
     def update(self, delta):
@@ -41,6 +55,9 @@ class SnakeWindow(arcade.Window):
     def on_draw(self):
         arcade.start_render()
         self.snake_sprite.draw()
+
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
 
 def main():
     window = SnakeWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
